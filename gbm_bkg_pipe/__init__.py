1fe24fea7bcf1b5ebe0e97c6f0abca382f8edb3b
Felix Kunzweiler <felix.kunzweiler@tum.de>
2020-09-19 10:58:09 +0200

    Add CreateBkgConfig to init

--- gbm_bkg_pipe/__init__.py
+++ gbm_bkg_pipe/__init__.py
@@ -3,6 +3,7 @@ from gbm_bkg_pipe.bkg_fit_remote_handler import GBMBackgroundModelFit
 from gbm_bkg_pipe.trigger_search import TriggerSearch
 from gbm_bkg_pipe.balrog_handler import LocalizeTriggers
 from gbm_bkg_pipe.report import CreateReportDate
+from gbm_bkg_pipe.bkg_fit_remote_handler import CreateBkgConfig
 
 from ._version import get_versions
 

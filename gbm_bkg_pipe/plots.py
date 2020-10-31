b1fabefe6062830ce7a699adfba99bd5d9f5fdc8
Felix Kunzweiler <felix.kunzweiler@tum.de>
2020-10-31 14:37:34 +0100

    Fix missing config

--- gbm_bkg_pipe/plots.py
+++ gbm_bkg_pipe/plots.py
@@ -19,6 +19,7 @@ from gbm_bkg_pipe.utils.plot_utils import (
     swift_gbm_plot,
 )
 from gbm_bkg_pipe.utils.file_utils import if_dir_containing_file_not_existing_then_make
+from gbm_bkg_pipe.configuration import gbm_bkg_pipe_config
 
 base_dir = os.path.join(os.environ.get("GBMDATA"), "bkg_pipe")
 _valid_gbm_detectors = np.array(gbm_bkg_pipe_config["data"]["detectors"]).flatten()

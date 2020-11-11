13dc94237942bdd3a8c7e5ff13fdf7ce8a762088
Felix Kunzweiler <felix.kunzweiler@tum.de>
2020-11-11 15:07:20 +0100

    Split cron job in new date and backprocessing

--- bin/bkg_pipe_cron_range.sh
+++ bin/bkg_pipe_cron_range.sh
@@ -2,4 +2,4 @@
 #/home/fkunzwei/data1/envs/bkg_pipe/bin/python -m luigi --module gbm_bkg_pipe CreateReportDate --workers 32 --date $(date +'%Y-%m-%d')
 source /home/fkunzwei/data1/envs/bkg_pipe/bin/activate
 
-/home/fkunzwei/data1/envs/bkg_pipe/bin/python -m luigi --workers 32 --module gbm_bkg_pipe CreateReportDate --date $(date +'%Y-%m-%d')
+/home/fkunzwei/data1/envs/bkg_pipe/bin/python -m luigi --workers 32 --module gbm_bkg_pipe RangeDailyBase --of CreateReportDate --days-back=5 --stop $(date +'%Y-%m-%d') --reverse

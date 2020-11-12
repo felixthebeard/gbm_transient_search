b0af4ae87f39ed03e9675c0155049ef574fa9e3d
Felix Kunzweiler <felix.kunzweiler@tum.de>
2020-11-12 10:26:27 +0100

    Fix midnight cron

--- bin/bkg_pipe_cron.sh
+++ bin/bkg_pipe_cron.sh
@@ -2,4 +2,4 @@
 #/home/fkunzwei/data1/envs/bkg_pipe/bin/python -m luigi --module gbm_bkg_pipe CreateReportDate --workers 32 --date $(date +'%Y-%m-%d')
 source /home/fkunzwei/data1/envs/bkg_pipe/bin/activate
 
-/home/fkunzwei/data1/envs/bkg_pipe/bin/python -m luigi --workers 32 --module gbm_bkg_pipe CreateReportDate --date $(date +'%Y-%m-%d')
+/home/fkunzwei/data1/envs/bkg_pipe/bin/python -m luigi --workers 32 --module gbm_bkg_pipe CreateReportDate --date $(date --date='-1 day' +'%Y-%m-%d')

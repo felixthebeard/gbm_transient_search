0a505c0563e1429dd948f235d1f9c855fa4c41c9
Felix Kunzweiler <felix.kunzweiler@tum.de>
2021-03-27 19:17:34 +0100

    Cleanup

--- gbm_bkg_pipe/__init__.py
+++ gbm_bkg_pipe/__init__.py
@@ -3,11 +3,11 @@ import warnings
 warnings.simplefilter("ignore")
 
 from gbm_bkg_pipe.configuration import gbm_bkg_pipe_config
-from gbm_bkg_pipe.bkg_fit_remote_handler import GBMBackgroundModelFit
-from gbm_bkg_pipe.trigger_search import TriggerSearch
-from gbm_bkg_pipe.balrog_handler import LocalizeTriggers
-from gbm_bkg_pipe.report import CreateReportDate
-from gbm_bkg_pipe.bkg_fit_remote_handler import CreateBkgConfig
+from gbm_bkg_pipe.handlers.bkg_fit_remote_handler import GBMBackgroundModelFit
+from gbm_bkg_pipe.handlers.trigger_search import TriggerSearch
+from gbm_bkg_pipe.handlers.balrog_handler import LocalizeTriggers
+from gbm_bkg_pipe.handlers.report import CreateReportDate
+from gbm_bkg_pipe.handlers.bkg_fit_remote_handler import CreateBkgConfig
 
 from ._version import get_versions
 

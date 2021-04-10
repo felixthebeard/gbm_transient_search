c28f1bb8694c42f940fea165d7a3db4fbd0cbd71
Felix Kunzweiler <felix.kunzweiler@tum.de>
2021-04-10 13:54:59 +0200

    Move configureation to utils and add transient_detection config

--- gbm_bkg_pipe/utils/configuration.py
+++ gbm_bkg_pipe/utils/configuration.py
@@ -12,13 +12,6 @@ run_echans = [["0", "1", "2"], ["3", "4", "5"]]
 structure = {}
 
 structure["luigi"] = dict(n_workers=32)
-structure["phys_bkg"] = dict(
-    stan=dict(n_cores=16),
-    multinest=dict(
-        n_cores=4, path_to_python="/home/fkunzwei/data1/envs/bkg_pipe/bin/python"
-    ),
-    timeout=2 * 60 * 60,  # 1 hour
-)
 
 structure["remote_hosts_config"] = dict(
     priority_host="raven",
@@ -42,9 +35,30 @@ structure["remote_hosts_config"] = dict(
     ),
 )
 
+structure["ssh"] = dict(
+    master_socket_base_path="/home/fkunzwei/.ssh/master-socket/", nr_sockets=3
+)
 
 structure["download"] = dict(
     interval=5 * 60, max_time=6 * 60 * 60  # run every 5 min  # run for 6h
+)
+
+structure["data"] = dict(data_type="ctime", detectors=run_detectors, echans=run_echans)
+
+structure["phys_bkg"] = dict(
+    stan=dict(n_cores=16),
+    multinest=dict(
+        n_cores=4, path_to_python="/home/fkunzwei/data1/envs/bkg_pipe/bin/python"
+    ),
+    timeout=2 * 60 * 60,  # 1 hour
+)
+
+structure["transient_detection"] = dict(
+    min_separation=5,
+    model="l2",
+    min_significance=5,
+    min_dets_significance=5,
+    nr_dets=2,
 )
 
 structure["balrog"] = dict(
@@ -53,15 +67,9 @@ structure["balrog"] = dict(
     run_destination="remote",
 )
 
-structure["data"] = dict(data_type="ctime", detectors=run_detectors, echans=run_echans)
-
 structure["upload"] = dict(
     report=dict(interval=30, max_time=2 * 60 * 60),
     plot=dict(interval=30, max_time=2 * 60 * 60),
-)
-
-structure["ssh"] = dict(
-    master_socket_base_path="/home/fkunzwei/.ssh/master-socket/", nr_sockets=3
 )
 
 

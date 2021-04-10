0fde4367ccbdf066a9a51a378af00178e9817cc0
Felix Kunzweiler <felix.kunzweiler@tum.de>
2021-04-10 14:00:29 +0200

    Fix import

--- gbm_bkg_pipe/__init__.py
+++ gbm_bkg_pipe/__init__.py
@@ -2,7 +2,7 @@ import warnings
 
 warnings.simplefilter("ignore")
 
-from gbm_bkg_pipe.configuration import gbm_bkg_pipe_config
+from gbm_bkg_pipe.utils.configuration import gbm_bkg_pipe_config
 from gbm_bkg_pipe.handlers.background import GBMBackgroundModelFit, CreateBkgConfig
 from gbm_bkg_pipe.handlers.transient_search import TransientSearch
 from gbm_bkg_pipe.handlers.localization import LocalizeTriggers

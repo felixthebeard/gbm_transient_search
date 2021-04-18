ebcfef1e5c68272585e1462687ca4818046108ba
Felix Kunzweiler <felix.kunzweiler@tum.de>
2021-04-18 16:53:39 +0200

    Cleanup versioneer

--- gbm_transient_search/__init__.py
+++ gbm_transient_search/__init__.py
@@ -3,12 +3,14 @@ import warnings
 warnings.simplefilter("ignore")
 
 from gbm_transient_search.utils.configuration import gbm_transient_search_config
-from gbm_transient_search.handlers.background import GBMBackgroundModelFit, CreateBkgConfig
+from gbm_transient_search.handlers.background import (
+    GBMBackgroundModelFit,
+    CreateBkgConfig,
+)
 from gbm_transient_search.handlers.transient_search import TransientSearch
 from gbm_transient_search.handlers.localization import LocalizeTriggers
 from gbm_transient_search.handlers.report import CreateReportDate
 
 from ._version import get_versions
-
-__version__ = get_versions()["version"]
+__version__ = get_versions()['version']
 del get_versions
